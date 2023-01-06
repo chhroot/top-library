36c32a26e4ded656c67a0bc29f6e694cacd82ad2
amruthkiran <amruthkiran47@gmail.com>
2023-01-06 22:18:52 +0530

    minor

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 //     this.pages = pages
 //     this.read = read
 //   }
-  
+
 //   function addBookToLibrary() {
 //     const book1 = new Book('test', 'test', 'test', 'test')
 //     myLibrary.push(book1)
@@ -15,10 +15,27 @@ myLibrary = [
     title: "The Hobbit",
     author: "J.J Tolkien",
     pages: 295,
+    status: "read",
+  },
+  {
+    title: "The Last Wish",
+    author: "Andrzej Sapkowski",
+    pages: 400,
+    status: "read",
+  },
+  {
+    title: "The Hobbit",
+    author: "J.J Tolkien",
+    pages: 295,
+    status: "read",
+  },
+  {
+    title: "The Hobbit",
+    author: "J.J Tolkien",
+    pages: 295,
+    status: "read",
   },
 ];
-
-
 
 const booksContainer = document.getElementById("books");
 
@@ -35,9 +52,31 @@ for (const book of myLibrary) {
   const pagesElement = document.createElement("p");
   pagesElement.textContent = `${book.pages} pages`;
 
+  const readElement = document.createElement("p");
+  readElement.textContent = book.status;
+
   bookElement.appendChild(titleElement);
   bookElement.appendChild(authorElement);
   bookElement.appendChild(pagesElement);
+  bookElement.appendChild(readElement);
 
   booksContainer.appendChild(bookElement);
 }
+
+//form functions
+function showForm() {
+  var formContainer = document.getElementById("form-container");
+  formContainer.style.display = "flex";
+}
+
+function closeForm() {
+  var formContainer = document.getElementById("form-container");
+  formContainer.style.display = "none";
+}
+
+// Close the form when the escape key is pressed
+document.addEventListener("keyup", function (event) {
+  if (event.keyCode === 27) {
+    closeForm();
+  }
+});
