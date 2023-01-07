88447f0d7a81da3fd388a85855413392050d114b
amruthkiran <amruthkiran47@gmail.com>
2023-01-07 16:12:55 +0530

    checkpoint

--- index.js
+++ index.js
@@ -1,69 +1,110 @@
-// function Book(title, author, pages, read) {
-//     this.title = title
-//     this.author = author
-//     this.pages = pages
-//     this.read = read
-//   }
-
-//   function addBookToLibrary() {
-//     const book1 = new Book('test', 'test', 'test', 'test')
-//     myLibrary.push(book1)
-//   }
-
 myLibrary = [
   {
-    title: "The Hobbit",
-    author: "J.J Tolkien",
-    pages: 295,
-    status: "read",
+    id:1,
+    title:"The Hobbit",
+    author: "Tolkeins",
+    pages: 56,
+    status: true,
+
   },
+
   {
-    title: "The Last Wish",
-    author: "Andrzej Sapkowski",
-    pages: 400,
-    status: "read",
+    id:2,
+    title:"sdsd",
+    author: "sdds",
+    pages: 56,
+    status: true,
+
   },
+
   {
-    title: "The Hobbit",
-    author: "J.J Tolkien",
-    pages: 295,
-    status: "read",
-  },
-  {
-    title: "The Hobbit",
-    author: "J.J Tolkien",
-    pages: 295,
-    status: "read",
+    id:3,
+    title:"The Hobbit",
+    author: "Tolkeins",
+    pages: 56,
+    status: true,
+
   },
 ];
 
-const booksContainer = document.getElementById("books");
+let nextId = 4;
 
-for (const book of myLibrary) {
-  const bookElement = document.createElement("div");
-  bookElement.classList.add("book");
+class Book {
+  constructor(title, author, pages, status) {
+    this.id = nextId++; 
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+  }
 
-  const titleElement = document.createElement("h2");
-  titleElement.textContent = book.title;
-
-  const authorElement = document.createElement("h3");
-  authorElement.textContent = book.author;
-
-  const pagesElement = document.createElement("p");
-  pagesElement.textContent = `${book.pages} pages`;
-
-  const readElement = document.createElement("p");
-  readElement.textContent = book.status;
-
-  bookElement.appendChild(titleElement);
-  bookElement.appendChild(authorElement);
-  bookElement.appendChild(pagesElement);
-  bookElement.appendChild(readElement);
-
-  booksContainer.appendChild(bookElement);
 }
 
-//form functions
+
+// DISPLAYS THE BOOKS
+function displayLibrary() {
+  const booksContainer = document.getElementById("books");
+  booksContainer.innerHTML = "";
+
+  for (const book of myLibrary) {
+    const bookElement = document.createElement("div");
+    bookElement.classList.add("book");
+    bookElement.setAttribute('data-id', book.id);
+  
+    const titleElement = document.createElement("h2");
+    titleElement.textContent = book.title;
+    titleElement.classList.add('title');
+  
+    const authorElement = document.createElement("h3");
+    authorElement.textContent = book.author;
+  
+    const pagesElement = document.createElement("p");
+    pagesElement.textContent = `${book.pages} pages`;
+    
+    const statusContainer = document.createElement("div");
+    statusContainer.classList.add('status-container');
+
+    const readElement = document.createElement("p");
+    readElement.textContent = book.status;
+
+    const checkBox = document.createElement("p");
+    checkBox.textContent = "TOGGLE";
+    checkBox.setAttribute('id', 'toggle-status');
+
+    statusContainer.appendChild(readElement);
+    statusContainer.appendChild(checkBox);
+
+  
+    bookElement.appendChild(titleElement);
+    bookElement.appendChild(authorElement);
+    bookElement.appendChild(pagesElement);
+    bookElement.appendChild(statusContainer);
+  
+    booksContainer.appendChild(bookElement);
+  }
+}
+
+//PUSH NEW BOOK TO LIBRARY
+function addBookToLibrary(title , author, pages, read) {
+  const book = new Book(title, author, pages, read);  
+  myLibrary.push(book)
+}
+
+function toggleStatus(event) {
+  let bookEl = event.target.closest('.book');
+  let id = bookEl.getAttribute('data-id');
+  let book = myLibrary.find(book => String(book.id) === id);
+
+  // book.status = !book.status;
+  
+  book.status = !book.status;
+
+  displayLibrary();
+  
+}
+
+
+//FORM FUNCTIONS
 function showForm() {
   var formContainer = document.getElementById("form-container");
   formContainer.style.display = "flex";
@@ -74,9 +115,56 @@ function closeForm() {
   formContainer.style.display = "none";
 }
 
-// Close the form when the escape key is pressed
+
 document.addEventListener("keyup", function (event) {
   if (event.keyCode === 27) {
     closeForm();
   }
 });
+
+function validateForm(event){
+  
+  const titleInput = document.querySelector('#book-name');
+  const authorInput = document.querySelector('#author-name');
+  const pagesInput = document.querySelector('#pages');
+  const checkbox = document.querySelector('input[name="checkbox"]');
+
+  console.log(checkbox.checked);
+  if (titleInput.value !== '' && authorInput.value !== '' && pagesInput.value !== '' && pagesInput.value > 0) {
+    if (checkbox.checked) {
+      addBookToLibrary(titleInput.value, authorInput.value, pagesInput.value, true);
+      closeForm();
+    } else {
+      addBookToLibrary(titleInput.value, authorInput.value, pagesInput.value, false);
+      closeForm();
+    }
+
+  }
+
+  displayLibrary();
+}
+
+//EVENT LISTENERS
+function listenClicks(){
+  
+  document.addEventListener('click', (event) => {
+    const { target } = event;
+    
+
+    if(target.id == 'add-book'){
+      validateForm(event);
+    }
+
+    else if(target.id == 'toggle-status'){
+      toggleStatus(event);
+    }
+
+  });
+
+  
+}
+
+
+displayLibrary();
+
+listenClicks();
